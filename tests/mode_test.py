feae347627a2b74d211566abc942f27a800a1075
ppbrs <meemkcfr@proton.me>
2024-08-11 08:24:41 -0700

    Clean-up ModeTest

--- tests/mode_test.py
+++ tests/mode_test.py
@@ -1,50 +1,52 @@
-
+"""Tests for Mode class."""
 import unittest
 
 from hotkey.mode import Mode
 
 
-class Mode_test(unittest.TestCase):
+class ModeTest(unittest.TestCase):
+    """Hold tests for Mode class."""
 
-    def test_specific(self):
+    def test_specific(self) -> None:
+        """Test Mode with specific parameters."""
 
         sut = Mode(alt=False, alt_gr=False, ctrl=False, ctrl_gr=False, shift=False, win=False)
-        self.assertEqual(str(sut), '', 'Error in string representation')
+        self.assertEqual(str(sut), "", "Error in string representation")
 
         sut = Mode(alt=True, alt_gr=False, ctrl=False, ctrl_gr=False, shift=False, win=False)
-        self.assertEqual(str(sut), 'alt', 'Error in string representation')
+        self.assertEqual(str(sut), "alt", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=True, ctrl=False, ctrl_gr=False, shift=False, win=False)
-        self.assertEqual(str(sut), 'alt-gr', 'Error in string representation')
+        self.assertEqual(str(sut), "alt-gr", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=False, ctrl=True, ctrl_gr=False, shift=False, win=False)
-        self.assertEqual(str(sut), 'ctrl', 'Error in string representation')
+        self.assertEqual(str(sut), "ctrl", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=False, ctrl=False, ctrl_gr=True, shift=False, win=False)
-        self.assertEqual(str(sut), 'ctrl-gr', 'Error in string representation')
+        self.assertEqual(str(sut), "ctrl-gr", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=False, ctrl=False, ctrl_gr=False, shift=True, win=False)
-        self.assertEqual(str(sut), 'shift', 'Error in string representation')
+        self.assertEqual(str(sut), "shift", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=False, ctrl=False, ctrl_gr=False, shift=False, win=True)
-        self.assertEqual(str(sut), 'win', 'Error in string representation')
+        self.assertEqual(str(sut), "win", "Error in string representation")
 
         sut = Mode(alt=True, alt_gr=False, ctrl=True, ctrl_gr=False, shift=False, win=False)
-        self.assertEqual(str(sut), 'ctrl + alt', 'Error in string representation')
+        self.assertEqual(str(sut), "ctrl + alt", "Error in string representation")
 
         sut = Mode(alt=True, alt_gr=False, ctrl=False, ctrl_gr=False, shift=True, win=False)
-        self.assertEqual(str(sut), 'alt + shift', 'Error in string representation')
+        self.assertEqual(str(sut), "alt + shift", "Error in string representation")
 
         sut = Mode(alt=False, alt_gr=False, ctrl=True, ctrl_gr=False, shift=True, win=False)
-        self.assertEqual(str(sut), 'ctrl + shift', 'Error in string representation')
+        self.assertEqual(str(sut), "ctrl + shift", "Error in string representation")
 
-    def test_is_expected(self):
-
+    def test_eq(self) -> None:
+        """Test Mode.__eq__()."""
         # List of tuples. Each tuple:
         # 1. expected mode
         # 2. list of matching modes
         # 3. list of not matching modes
-        test_sets = [
+        test_sets: list[tuple[Mode, list[Mode], list[Mode]]] = [
             (Mode(alt=True),
              [Mode(alt=True), ],
              [Mode(), Mode(alt_gr=True), Mode(ctrl=True), Mode(ctrl_gr=True), Mode(shift=True), ],
@@ -72,16 +74,19 @@ class Mode_test(unittest.TestCase):
             modes_matching = test_set[1]
             modes_nonmatching = test_set[2]
             for mode_matching in modes_matching:
-                self.assertTrue(mode_matching.is_expected(mode_expected),
-                                f'Pressed `{mode_matching}` must match expected `{mode_expected}`')
+                self.assertTrue(
+                    mode_matching == mode_expected,
+                    f"Pressed `{mode_matching}` must match expected `{mode_expected}`")
             for mode_nonmatching in modes_nonmatching:
-                self.assertFalse(mode_nonmatching.is_expected(mode_expected),
-                                 f'Pressed `{mode_nonmatching}` should not match expected `{mode_expected}`')
+                self.assertFalse(
+                    mode_nonmatching == mode_expected,
+                    f"Pressed `{mode_nonmatching}` should not match expected `{mode_expected}`")
 
 
-def main():
+def main() -> None:
+    """Standalone main, used for debugging."""
     unittest.main()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
