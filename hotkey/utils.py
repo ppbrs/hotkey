817430c52c48118a363819e95d972bf6953ddb5d
ppbrs <meemkcfr@proton.me>
2024-08-11 07:38:36 -0700

    Clean-up utils.py

--- hotkey/utils.py
+++ hotkey/utils.py
@@ -1,36 +1,12 @@
-"""
-"""
+"""Helper functions."""
 import os
-import time
-
-from hotkey.error import Error
 
 
-class Utils_error(Error):
-    pass
-
-
-def clear_screen():
-
-    def clear_nt():
-        os.system('cls')
-
-    def clear_posix():
-        os.system('clear')
-
-    if os.name == 'nt':
-        clear_nt()
-    elif os.name == 'posix':
-        clear_posix()
+def clear_screen() -> None:
+    """Clear terminal screen."""
+    if os.name == "nt":
+        os.system("cls")
+    elif os.name == "posix":
+        os.system("clear")
     else:
-        raise Utils_error("Unsupported OS.")
-
-
-if __name__ == "__main__":
-
-    print('this is', __name__)
-    time.sleep(1.0)
-
-    clear_screen()
-    print('screen was cleared')
-    time.sleep(1.0)
+        raise RuntimeError("Unsupported OS.")
