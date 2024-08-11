216a18394ea1bec5fb37e2c89b0f561bbc3a3e13
ppbrs <meemkcfr@proton.me>
2024-08-11 08:18:52 -0700

    Clean-up Key and KeyTest

--- hotkey/key.py
+++ hotkey/key.py
@@ -1,58 +1,59 @@
-""" Key abstraction.
-"""
+"""Key class."""
 from __future__ import annotations
 
 import random
-
-from hotkey.error import Error
+from typing import Any
 
 
 class Key:
-    """ An abstraction representing a key of a keyboard that is chosen randomly from all keys
-    of a subset.
-
-    Note: This class can be simplified because all keys created by the application are now specific.
     """
-
-    class KeyError(Error):
-        """ An exception thrown when a Key instance cannot be created.
-        """
-
+    An abstraction representing a key of a keyboard that is chosen randomly from all keys
+    in a subset.
+    """
     letters_list = [chr(c) for c in range(ord("a"), ord("z") + 1)]
     digits_list = [chr(c) for c in range(ord("0"), ord("9") + 1)]
-    symbols_list = ("`", "-", "=",
-                    "[", "]", "\\",
-                    ";", "\"",
-                    ",", ".", "/",
-                    "space")
+    symbols_list = ["`", "-", "=", "[", "]", "\\", ";", "\"", ",", ".", "/", "space"]
+    symbol_key_mapping = {
+        "~": "`",
+        "!": "1",
+        "@": "2",
+        "#": "3",
+        "$": "4",
+        "%": "5",
+        "^": "6",
+        "&": "7",
+        "*": "8",
+        "(": "9",
+        ")": "0",
+        "_": "-",
+        "+": "=",
+        "?": "/",
+    }
 
-    def __init__(self,
-                 letters: bool = False, digits: bool = False, symbols: bool = False,
-                 chars: bool = False, nonchars: bool = False,
-                 esc: bool = False, backspace: bool = False, enter: bool = False, tab: bool = False,
-                 delete: bool = False,
-                 specific: str | None = None) -> None:
+    def __init__(
+        self,
+        letters: bool = False, digits: bool = False, symbols: bool = False,
+        chars: bool = False, nonchars: bool = False,
+        esc: bool = False, backspace: bool = False, enter: bool = False, tab: bool = False,
+        delete: bool = False,
+        specific: str | None = None
+    ) -> None:
         """Chooses one key randomly according to the arguments.
 
-        Args:
-            letters: key may be  chosen from letters from 'a' to 'z'.
-            digits: key may be  chosen from digits from '0' to '9'
-            symbols: key may be  chosen from any chars
-                available on the keyboard except letters and digits.
-            esc: Escape-key may be chosen.
-            backspace: Backspace-key may be chosen.
-            enter: Enter-key may be chosen.
-            tab: Tab-key may be chosen.
-            delete: Delete-key may be chosen.
-            chars: Any letter or digit or symbol may be chosen.
-            nonchars: Esc, enter, backspace, tab, or delete may by chosen.
-            specific: Specific key
+        :param letters: key may be  chosen from letters from 'a' to 'z'.
+        :param digits: key may be  chosen from digits from '0' to '9'
+        :param symbols: key may be  chosen from any chars
+            available on the keyboard except letters and digits.
+        :param esc: Escape-key may be chosen.
+        :param backspace: Backspace-key may be chosen.
+        :param enter: Enter-key may be chosen.
+        :param tab: Tab-key may be chosen.
+        :param delete: Delete-key may be chosen.
+        :param chars: Any letter or digit or symbol may be chosen.
+        :param nonchars: Esc, enter, backspace, tab, or delete may by chosen.
+        :param specific: Specific key
 
-        Returns:
-
-        Raises:
-            KeyError: when no argument is provided.
-
+        :raises ValueError: when no argument is provided.
         """
         # pylint: disable=too-many-arguments
 
@@ -77,37 +78,22 @@ class Key:
 
         if specific is not None:
             # Check that the key is valid.
-            dict_upper = {
-                "~": "`",
-                "!": "1",
-                "@": "2",
-                "#": "3",
-                "$": "4",
-                "%": "5",
-                "^": "6",
-                "&": "7",
-                "*": "8",
-                "(": "9",
-                ")": "0",
-                "_": "-",
-                "+": "=",
-                "?": "/",
-            }
-            if specific in dict_upper:
-                specific = dict_upper[specific]
+            if specific in self.symbol_key_mapping:
+                specific = self.symbol_key_mapping[specific]
             elif specific.isupper():
                 specific = specific.lower()
             variants.append(specific)
 
         if len(variants) == 0:
-            raise self.KeyError("Error in parameters.")
-
+            raise ValueError("Error in parameters.")
         self.key = random.choice(variants)
 
-    def __eq__(self, other: Key) -> bool:  # type: ignore[override]
+    def __eq__(self, other: Any) -> bool:
         assert isinstance(other, Key)
-        return (self.key == other.key) or (self.key.lower() == other.key) \
-            or (self.key == other.key.lower())
+        return (
+            self.key == other.key
+            or self.key.lower() == other.key
+            or self.key == other.key.lower())
 
     def __repr__(self) -> str:
         ret_val = str(self.key)
