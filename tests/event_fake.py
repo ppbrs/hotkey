b352024c0ce5d81a25be549842980494286af6d1
ppbrs <meemkcfr@proton.me>
2024-10-27 07:52:27 -0700

    Cleanup EventFake

--- tests/event_fake.py
+++ tests/event_fake.py
@@ -1,14 +1,27 @@
 """EventFake class."""
 
+import dataclasses
+
 import keyboard  # type: ignore
 
 
+@dataclasses.dataclass
 class EventFake:  # pylint: disable=R0903
-    """Fake keyboard event (keyboard.KeyboardEvent) for testing KeyCombo wait and callback."""
+    """
+    Fake keyboard event (keyboard.KeyboardEvent) for testing KeyCombo wait and callback.
+
+    Real keyboard.KeyboardEvent has more attributes, see
+    https://github.com/boppreh/keyboard?tab=readme-ov-file#keyboard.KeyboardEvent
+    """
+
+    event_type: str
+    """keyboard.KEY_DOWN | keyboard.KEY_UP"""
+    name: str
+    """E.g. 'a', '1', 'alt'"""
 
     def __init__(
         self,
-        event_type: str,  # keyboard.KEY_DOWN | keyboard.KEY_UP
+        event_type: str,
         name: str,
     ) -> None:
         assert event_type in (keyboard.KEY_DOWN, keyboard.KEY_UP)
@@ -18,7 +31,7 @@ class EventFake:  # pylint: disable=R0903
     def __repr__(self) -> str:
         type_repr = "unknown"
         if self.event_type == keyboard.KEY_DOWN:
-            type_repr = "dn"
-        if self.event_type == keyboard.KEY_UP:
+            type_repr = "down"
+        elif self.event_type == keyboard.KEY_UP:
             type_repr = "up"
-        return f"Event({type_repr}, {self.name})"
+        return f"{{{type_repr}, {self.name}}}"
